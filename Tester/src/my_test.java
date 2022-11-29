d0178a05a7cec07c85e7c846cb42d1aaf2f11403
test-demo-user100 <103204018+test-demo-user100@users.noreply.github.com>
2022-11-29 12:33:35 +0530

    Create my_test.java

---
+++ Tester/src/my_test.java
@@ -0,0 +1,14 @@
+class Test {
+
+  @Override
+  public String toString() {
+    return "pipeline integration";
+  }
+}
+
+class Main {
+  public static void main(String[] args) {
+    Test obj = new Test();
+    System.out.println(obj);
+  }
+}
